43f0a6371e6aa62c7c3a944b2e321fc39991793d
yeonsong <140032559+bunchofyeon@users.noreply.github.com>
2026-01-05 17:48:41 +0900

    Refactor K번째수 solution method
    Refactor the solution method to improve readability and maintainability.

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -14,22 +14,6 @@ class Solution {
     }
 }
 
-import java.util.Arrays;
-
-class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        for(int n = 0; n < commands.length; n++) {
-            
-            int[] list = Arrays.copyOfRange(array, commands[n][0]-1, commands[n][1]);
-            Arrays.sort(list);
-            answer[n] = list[commands[n][2]-1];
-
-        }
-        return answer;
-    }
-}
-
 /*
 import java.util.Arrays;
 class Solution {
